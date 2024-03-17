b0de1052cbc4f92b570953f0df12a83d2960a03f
Macan Mehri <macanmehri@gmail.com>
2024-03-17 14:39:46 +0330

    Created two serializers for Post and Category models

---
+++ blog/serializers.py
@@ -0,0 +1,35 @@
+from .models import Category, Post
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Category
+
+        fields = (
+            'id',
+            'title',
+            'description',
+            'is_active',
+            'created_date',
+            'updated_date',
+        )
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Post
+        category = CategorySerializer
+
+        fields = (
+            'id',
+            'title',
+            'description',
+            'is_active',
+            'created_date',
+            'updated_date',
+        )
