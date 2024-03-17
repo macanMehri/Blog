c5452aacf99ffa37ac4307db56aaa16eb0e44494
Macan Mehri <macanmehri@gmail.com>
2024-03-17 13:41:58 +0330

    Created local_settings file to store secret settings in it and added it to .gitignore - Created a sample_settings file for user to create her/his own local_settings

--- website/settings.py
+++ website/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,14 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f7hit4pzdlfoev%u!x1i)m1$@w0!ot(3d+lsn5qs&35s-92hcm'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
