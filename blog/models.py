46c28bc72633f96468c9f5bf218bd928131b1e22
Macan Mehri <macanmehri@gmail.com>
2024-03-17 13:57:06 +0330

    Created a base model and two models of category and post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,80 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name='Is Active'
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created Date'
+    )
+
+    updated_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated Date'
+    )
+
+
+    class Meta:
+        abstract = True
+
+
+    def __str__(self) -> str:
+        raise NotImplementedError('You did not override the string method!')
+
+
+
+class Category(BaseModel):
+
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Title'
+    )
+
+    description = models.TextField(
+        blank=False,
+        verbose_name='Description'
+    )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(BaseModel):
+
+    title = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Title'
+    )
+
+    description = models.TextField(
+        blank=False,
+        verbose_name='Description'
+    )
+
+    category = models.ForeignKey(
+        Category,
+        null=False,
+        on_delete=models.deletion.CASCADE,
+        verbose_name='Category'
+    )
+
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+    def __str__(self) -> str:
+        return f'{self.category} - {self.title} : {self.description}'
