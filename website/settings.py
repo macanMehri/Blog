7a84d8a0f65781d5ccf378e120478bc1e311a5cf
Macan Mehri <macanmehri@gmail.com>
2024-03-17 14:41:29 +0330

    Updated settings and added trusted apps to installed apps

--- website/settings.py
+++ website/settings.py
@@ -32,6 +32,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # MyApps
     'blog.apps.BlogConfig',
+    # TrustedApps
+    'django_filters',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +120,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHONTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ) if DEBUG else (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
