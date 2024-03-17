a3bfbfbcc89e4196b5ddb331473299dd841c42b4
Macan Mehri <macanmehri@gmail.com>
2024-03-17 14:44:25 +0330

    Created viewset classes for api vide

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters, permissions
+from .serializers import CategorySerializer, PostSerializer
+from .models import Category, Post
 
-# Create your views here.
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Category.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Post.objects.filter(is_active=True,).order_by('pk')
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
